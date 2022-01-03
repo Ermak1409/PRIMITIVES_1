4c3e523b7a5d23f7afbdcb427b24e8ac70bf48d2
Ermak <ermak1409@gmail.com>
2022-01-03 16:46:09 +0300

    Код

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+       // Переменные и входящие данные
+        int ticketPrice = 40;
+        int coef = 20;
+
+        //Расчет
+       int mili = ticketPrice / coef;
+
+       // Вывод
+        System.out.print("Ваши мили " + mili);
+    }
+}
